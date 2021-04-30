515c932c903148394d1fc48f81317384cec46a32
MuhammedKK <khaloood2281999@gmail.com>
2021-04-30 06:00:36 +0200

    last updated

--- main.js
+++ main.js
@@ -2,10 +2,7 @@
  * Constants
  */
 let content = document.querySelector(".content .container .lessons"); // All Lessons Content
-
-
-
-
+let wordscont = document.querySelector(".content .container .words");
 
 /*******************************************************************************/
 
@@ -14,26 +11,97 @@ let content = document.querySelector(".content .container .lessons"); // All Les
 */
 let data = [
     {
+        title: "Sports",
+        desc: `
+            <div class="card-text desc">
+                <div class="eng-sen">I love footbal</div>
+                <div class="ar-sen">انا احب الكورة</div>
+            </div>
+            <div class="card-text desc">
+                <div class="eng-sen">I love footbal</div>
+                <div class="ar-sen">انا احب الكورة</div>
+            </div>
+            <div class="card-text desc">
+                <div class="eng-sen">I love footbal</div>
+                <div class="ar-sen">انا احب الكورة</div>
+            </div>
+        `,
         lessonNum: 1,
-        title: "Lesson 1",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, dolores. Perferendis optio sapiente dolores facere, necessitatibus consequuntur mollitia aliquam harum debitis corporis dolor veniam unde odit, accusantium eum esse modi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, dolores. Perferendis optio sapiente dolores facere, necessitatibus consequuntur mollitia aliquam harum debitis corporis dolor veniam unde odit, accusantium eum esse modi."
     },
     {
+        title: "Reading",
+        desc: `
+            <div class="card-text desc">
+                <div class="eng-sen">I love books so much</div>
+                <div class="ar-sen">انا احب الكتب جدا</div>
+            </div>
+        `,
         lessonNum: 2,
-        title: "Lesson 2",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, dolores. Perferendis optio sapiente dolores facere, necessitatibus consequuntur mollitia aliquam harum debitis corporis dolor veniam unde odit, accusantium eum esse modi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, dolores. Perferendis optio sapiente dolores facere, necessitatibus consequuntur mollitia aliquam harum debitis corporis dolor veniam unde odit, accusantium eum esse modi."
     },
     {
+        title: "Foods",
+        desc: `
+            <div class="card-text desc">
+                <div class="eng-sen">I love food so much</div>
+                <div class="ar-sen">انا احب الاكل جدا</div>
+            </div>
+        `,
         lessonNum: 3,
-        title: "Lesson 3",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, dolores. Perferendis optio sapiente dolores facere, necessitatibus consequuntur mollitia aliquam harum debitis corporis dolor veniam unde odit, accusantium eum esse modi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, dolores. Perferendis optio sapiente dolores facere, necessitatibus consequuntur mollitia aliquam harum debitis corporis dolor veniam unde odit, accusantium eum esse modi."
-    }
+    },
 ];
-
+/**
+* Array Of Data Words
+*/
+let words = [
+    {
+        lessonName: "Sports",
+        engWord: "Sports",
+        arWord: "رياضة",
+        ex: `
+            <div class="card-text desc">
+                <div class="eng-sen">Sport is my life</div>
+                <div class="ar-sen">الرياضة هي حياتي</div>
+            </div>
+        `,
+        num: 1,
+    },
+];
 
 /**
  * Functions
  */
+
+/* Words Section */
+
+let showWords = () => {
+    $(".lessons").css("display", "none"); // hide content lessons
+    $(".words").fadeIn();
+    for (let i = 0; i < words.length; i++) {
+        let wordSection = document.createElement("div");
+        wordSection.classList.add("card")
+        wordSection.innerHTML = `
+            <div class="card-header">
+                <div class="card-title">words of lesson: 
+                <span class="title">
+                    ${words[i].lessonName}</span>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="card-text desc">
+                    <div class="eng-sen">${words[i].engWord}</div>
+                    <div class="ar-sen">${words[i].arWord}</div>
+                </div>
+                <h5 class="ex">Examples</h5>
+                ${words[i].ex}
+            </div>
+            <span class="num">${words[i].num}</span>
+        `;
+        wordscont.append(wordSection);
+    }
+}
+
+
+
 
 // Function To Show All Lessons
 (function ShowContents() {
@@ -42,23 +110,54 @@ let data = [
         lesson.classList.add(`lesson${i + 1}`);
         lesson.classList.add("card")
         lesson.innerHTML = `
-            <h2 class="cont-title card-header">${data[i].title}</h2>
-            <p class="cont-desc card-text">${data[i].desc}</p>
-            <a href="#" class="num btn btn-primary">${data[i].lessonNum}</a>
+            <div class="card-header">
+                <div class="card-title">lesson Name: 
+                <span class="title">
+                    ${data[i].title}
+                </span>
+                </div>
+            </div>
+            <div class="card-body">
+                ${data[i].desc}
+            </div>
+            
+            <span class="num">${data[i].lessonNum}</span>
         `;
         content.append(lesson)
+        // // show words of lesson
+        // $(".show-words").on("click", function() {
+        //     // e.preventDefault();
+        //     console.log(words[i]);
+        // });
     }
 })();
 
-// get Lesson by Lesson Menu
+
+
+// show lesson one in opennig
+$('div.lesson1').fadeIn().addClass("active");
+
+// // get Lesson by Lesson Menu
 
 $(".lessons-menu a").on("click", function () {
-    $(".lessons div").fadeOut();
+    $(".lessons .card").fadeOut().removeClass("active");
     $("." + $(this).data("lesson")).delay(500);
-    $("." + $(this).data("lesson")).fadeIn();
+    $("." + $(this).data("lesson")).fadeIn().addClass("active");
     // console.log($(this).data("lesson"));
 });
 
+// num of lesson
+// let lessonNum = $("div.active .num").text();
+// console.log($("div.active .num").text());
+$(".show-words").on("click", () => {
+    // let lessonNum = $("div.active .num").text();
+    
+    // // get lesson words
+    // for(let i = 0; i < words.length; i++) {
+        
+    // }
+    showWords();
+})
 
 
 
--- quiz.js
+++ quiz.js
@@ -1,71 +1,88 @@
 /**
  * Imports First Quiz : Rewrite The Sentence 
  */
+// let errs = [];
 
- let score = 0;
+// let score = 0;
+// // Define Current Quiz
+// let currentQuiz = 0;
+// const quizData = [
+//     {
+//         question: "انا احب الكورة جدا",
+//         answer: "I love football so much",
+//     },
+//     {
+//         question: "انا احب الاكل جدا",
+//         answer: "I love food so much",
+//     }
+// ];
 
- const quizData = [
-     {
-         question: "انا احب الكورة جدا",
-         answer: "I love football so much",
-     },
-     {
-         question: "انا احب الاكل جدا",
-         answer: "I love food so much",
-     }
- ];
- 
- // Define Current Quiz
- let currentQuiz = 0;
- const loadQuiz = () => {
- 
-     // Get The Data Of The Current Quiz
-     let currentQuizData = quizData[currentQuiz];
- 
-     // Get The Current Quiz Question
-     let question = document.querySelector(".quiz-question").textContent = currentQuizData.question;
- 
- };
 
- loadQuiz();
+// const loadQuiz = () => {
 
-// check the answer after submitted
+//     // Get The Data Of The Current Quiz
+//     let currentQuizData = quizData[currentQuiz];
 
-$(".re").on("click", () => {
-    let ans = $(".quiz-ans"); // get the answer
+//     // Get The Current Quiz Question
+//     let question = document.querySelector(".quiz-question").textContent = currentQuizData.question;
 
-    // check if answer right or not
-    if (ans.val() == quizData[currentQuiz].answer) {
-        score++;
-        console.log(score);
-    } else if (ans.val() == "") {
-        $(".err").fadeIn().addClass("alert alert-danger").text("Filed Cannot be empty").delay(1000).fadeOut(500);
-        return;
-        // console.log("not")
-    }
-    // get next question
-    currentQuiz++;
+// };
 
-    // Check If it's Not The Final Quiz
-    if (currentQuiz < quizData.length) {
-        ans.val("");
-        loadQuiz();
-    } else {
-        document.querySelector(".quizes-cont").innerHTML = `
-            <div class="card">
-                <div class="card-header">
-                Your Quiz Result
-                </div>
-                <div class="card-body">
-                    <h5 style="direction:ltr;text-align:center;" class="card-title quiz-question">Your Score is ${score + "/" + quizData.length}</h5>
-                    <label for="">Go To Next Quiz</label>
-                    <a href="#!" class="btn btn-primary send-ans">Next</a>
-                </div>
-            </div>
-        `;
-    }
-});
+// loadQuiz();
 
+// // check the answer after submitted
+
+// $(".re").on("click", () => {
+//     let ans = $(".quiz-ans"); // get the answer
+
+//     // check if answer right or not
+//     if (ans.val() == quizData[currentQuiz].answer) {
+//         score++;
+//         console.log(score);
+//     } else if (ans.val() == "") {
+//         $(".err").fadeIn().addClass("alert alert-danger").text("Filed Cannot be empty").delay(1000).fadeOut(500);
+//         return;
+//         // console.log("not")
+//     } else if(ans.val() != quizData[currentQuiz].answer) {
+//         errs.push({"wrong": ans.val(), "right": quizData[currentQuiz].answer, "quesNum":currentQuiz, "ques": quizData[currentQuiz].question }); // push wrong ans to array
+//     }
+//     // get next question
+//     currentQuiz++;
+
+//     // Check If it's Not The Final Quiz
+//     if (currentQuiz < quizData.length) {
+//         ans.val("");
+//         loadQuiz();
+//     } else {
+//         document.querySelector(".quizes-cont").innerHTML = `
+//             <div class="card ">
+//                 <div class="card-header">
+//                 Your Quiz Result
+//                 </div>
+//                 <div class="card-body">
+//                     <h5 style="direction:ltr;text-align:center;" class="card-title quiz-question">Your Score is ${score + "/" + quizData.length}</h5>
+//                     <label for="">Go To Next Quiz</label>
+//                     <a href="#" class="btn btn-primary re">Next</a>
+//                 </div>
+//             </div>
+//         `;
+//         if(errs.length != 0) {
+//             errs.forEach((err) => {
+//                 let errReport = `
+//                     <h5>Report Of Question ${err.quesNum+1} : <span>${err.ques}</span></h5>
+//                     <div class="report">
+//                         <div class="wrong alert alert-danger">Your Ansuer is => ${err.wrong}</div>
+//                         <div class="right alert alert-success">The Right Answer is => ${err.right}</div>
+//                     </div>
+//                 `;
+//                 $(".card-body").append(errReport);
+//             });
+//         }
+//         console.log(errs);
+//     }
+// });
+
+/*********************************************************************************************************/
 
 /**
  * Complete Quiz
@@ -84,9 +101,11 @@ let completeQuizData = [
 
 // define score
 let compScore = 0; 
-
 // Define Current Quiz
 let currentCompQuiz = 0;
+// catch errs
+let comErrs = [];
+
 const loadQuizComp = () => {
 
     // Get The Data Of The Current Quiz
@@ -101,7 +120,7 @@ loadQuizComp();
 
 // check the answer after submitted
 
-$(".send-ans").on("click", () => {
+$(".complete .comp").on("click", () => {
     let ans = $(".comp-ans"); // get the answer
 
     // check if answer right or not
@@ -112,6 +131,9 @@ $(".send-ans").on("click", () => {
         $(".err").fadeIn().addClass("alert alert-danger").text("Filed Cannot be empty").delay(1000).fadeOut(500);
         return;
         // console.log("not")
+    } else if(ans.val() != completeQuizData[currentCompQuiz].answer) {
+        // push err to array
+        comErrs.push({"wrong": ans.val(), "right": completeQuizData[currentCompQuiz].answer, "quesNum":currentCompQuiz, "ques": completeQuizData[currentCompQuiz].question })
     }
     // get next question
     currentCompQuiz++;
@@ -129,9 +151,30 @@ $(".send-ans").on("click", () => {
                 <div class="card-body">
                     <h5 style="direction:ltr;text-align:center;" class="card-title comp-question">Your Score is ${compScore + "/" + completeQuizData.length}</h5>
                     <label for="">Go To Next Quiz</label>
-                    <a href="#!" class="btn btn-primary send-ans">Next</a>
+                    <a href="#" class="btn btn-primary">Next</a>
                 </div>
             </div>
         `;
+        if(comErrs.length != 0) {
+            comErrs.forEach((err) => {
+                let errReport = `
+                    <h5>Report Of Question ${err.quesNum+1} : <span>${err.ques}</span></h5>
+                    <div class="report">
+                        <div class="wrong alert alert-danger">Your Ansuer is => ${err.wrong}</div>
+                        <div class="right alert alert-success">The Right Answer is => ${err.right}</div>
+                    </div>
+                `;
+                $(".card-body").append(errReport);
+            });
+        }
     }
+});
+
+// show quiz depend on data value 
+$(".quizes-menu a").on("click", function (e) {
+    e.preventDefault();
+    $(".quizes-cont .card").fadeOut().removeClass("active");
+    $("#" + $(this).data("quiz")).delay(500);
+    $("#" + $(this).data("quiz")).fadeIn().addClass("active");
+    // console.log($(this).data("quiz"));
 });
